4770c11f53204ad1f6822f61e7b76d398a94863a
nooonyan <ivan.gorsky.04@gmail.com>
2024-12-08 03:12:59 +0500

    add colors, fix bugs + add content divs

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Home from './assets/icons/Home.svg';
 import Brain from './assets/icons/Brain.svg';
 import Guides from './assets/icons/Guides.svg';
 import Data from './assets/icons/Data.svg';
-import Settings from './assets/icons/Settings.svg'
+import Settings from './assets/icons/Settings.svg';
+import Search from './assets/icons/Search.svg';
+
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -15,10 +17,10 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen w-screen bg-slate-400">
+    <div className="min-h-screen w-screen bg-white">
       {/* Sidebar */}
       <aside
-        className={`fixed top-0 left-0 h-screen text-white bg-slate-500 shadow-lg transition-all duration-300 ${
+        className={`fixed top-0 left-0 h-screen text-white bg-whiteturquoise shadow-lg transition-all duration-300 z-10 ${
           isMenuOpen ? "w-60" : "w-20"
         }`}
       >
@@ -100,28 +102,31 @@ function App() {
           isMenuOpen ? "pl-20" : "pl-20"
         }`}
       >
-        <div className="max-w-[1110px] mx-auto pt-4">
+        <div className="max-w-[1110px] mx-auto pt-4 h-[38px]">
           {/* Header */}
           <header className="flex justify-between items-start bg-none">
             {/* Search Input */}
-            <form class="max-w-md w-full">   
-                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                        </svg>
-                    </div>
-                    <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-                    <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            <form class="max-w-[540px] w-full border-2 border-turquoise rounded-full">   
+              <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
+              <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <img src={Search} alt="Search" className="w-6" />
                 </div>
+                <input
+                  type="search"
+                  id="default-search"
+                  class="block w-full p-4 ps-10 text-sm text-gray-900 bg-lightwhiteturquoise rounded-full focus:outline-none focus:ring-0"
+                  placeholder="Поиск..."
+                  required
+                />
+              </div>
             </form>
             {/* Login/Registration Buttons */}
             <div className="flex gap-3">
-              <button className="bg-blue-500 text-white w-[75px] h-11 rounded">
+              <button className="bg-white border-turquoise w-full h-11 rounded-full text-turquoise">
                 Вход
               </button>
-              <button className="bg-gray-200 w-[119px] h-11 rounded">
+              <button className="bg-turquoise text-white w-full h-11 rounded-full">
                 Регистрация
               </button>
             </div>
@@ -129,8 +134,12 @@ function App() {
 
           {/* Body */}
           <main className="mt-6">
-            <div className="bg-white p-6 rounded shadow-md mb-6">Блок 1</div>
-            <div className="bg-white p-6 rounded shadow-md">Блок 2</div>
+            <div className="bg-lightwhiteturquoise p-6 rounded shadow-md mb-6">
+              <h1 className="text-dark text-2xl">Добро пожаловать, username!</h1>
+            </div>
+            <div className="bg-lightwhiteturquoise p-6 rounded shadow-md">
+              <h1 className="text-dark text-2xl">Новости</h1>
+            </div>
           </main>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
   theme: {
     extend: {
       colors: {
+        'lightwhiteturquoise': "#E7F9F7",
+        'whiteturquoise': "#DFF2F0",
+        'addwhiteturquoise': "#92E5E2",
+        'turquoise': "#3AAFAA",
+        'lightturquoise': "#92E5E2",
+        'darkturquoise': "#2A7A78",
+        'dark': "#17242B",
         primary: "#4F46E5", // Основной цвет из вашего макета
         secondary: "#9333EA", // Второстепенный цвет
       },
